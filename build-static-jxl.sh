58fa1e210570d695a7677cdf6bb1a9d5087869a4
shengshampoo <shengzh@tutanota.com>
2025-09-10 12:37:46 +0800

    Update build-static-jxl.sh

--- build-static-jxl.sh
+++ build-static-jxl.sh
@@ -24,6 +24,7 @@ cd build0
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static --static -no-pie -s" ..
 ninja
 ninja install
+rm /usr/lib/libdeflate.so*
 
 # libtiff
 cd $WORKSPACE
