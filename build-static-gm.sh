740357ece189c365d3de125e674b4b9bbb948575
shengshampoo <shengzh@tutanota.com>
2025-12-27 22:41:39 +0800

    Update build-static-gm.sh

--- build-static-gm.sh
+++ build-static-gm.sh
@@ -25,7 +25,7 @@ cd build0
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static --static -no-pie -s" \
  -DJPEG_LIBRARY_RELEASE=/usr/lib/libjpeg.a -DZLIB_LIBRARY_RELEASE=/usr/lib/libz.a -DLIBSHARPYUV_LIBRARY=/usr/lib/libsharpyuv.a -DPNG_LIBRARY_RELEASE=/usr/lib/libpng.a ..
 sed -i 's@libtiff.a@libtiff.a /usr/lib/libzstd.a /usr/lib/libdeflate.a /usr/lib/libz.a /usr/lib/libwebp.a /usr/lib/libjpeg.a /usr/lib/libpng.a /usr/lib/liblzma.a /usr/lib/libsharpyuv.a@g' ./build.ninja
-sed -i 's@libSDL2-2.0.so.0.3200.8@libSDL2.a@g' ./build.ninja
+sed -i 's@libSDL2-2.0.so.0.3200.10@libSDL2.a@g' ./build.ninja
 ninja
 ninja install
 cd ../
