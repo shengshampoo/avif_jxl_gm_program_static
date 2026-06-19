e72ed4a477d74416d966058b78faf73cf2406ce5
shengshampoo <shengzh@tutanota.com>
2026-06-19 11:10:14 +0000

    Update build-static-avif.sh

--- build-static-avif.sh
+++ build-static-avif.sh
@@ -22,7 +22,7 @@ git clone https://chromium.googlesource.com/libyuv/libyuv.git
 cd libyuv
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static --static -no-pie -s" ..
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DLIBYUV_DISABLE_NEON=1 -DCMAKE_EXE_LINKER_FLAGS="-static --static -no-pie -s" ..
 ninja
 ninja install
 
